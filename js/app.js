021bf1d2ee19cb2ef3b7c0dd9284639f9f7e010e
AngieC <alovesj@hotmail.com>
2019-10-08 21:53:25 -0500

    javaScript and jQuery  functionality, let/var change

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-
 const vid = document.querySelector('video');
 const captions = document.querySelectorAll('.text .capt');
 const spans = document.querySelectorAll('span');
 
 //create and event listener that will trigger as the vid time updates, attach it to the vid element
 vid.addEventListener('timeupdate', () => {
-  for (let i = 0; i < captions.length; i++) {
+  for (var i = 0; i < captions.length; i++) {
     const caption = captions[i];
-    let start = caption.dataset.start;
-    let end = caption.dataset.end;
+    var start = caption.dataset.start;
+    var end = caption.dataset.end;
     if (vid.currentTime >= start && vid.currentTime < end) {
       caption.classList.add("highlight");
     } else {
@@ -17,31 +16,8 @@ vid.addEventListener('timeupdate', () => {
   }
 });
 
-spans.addEventListener('click',() => {
-  for (let i = 0; i < spans.length; i++) 
-
-
+$(spans).click(function(event) {
+  const captionStart = parseFloat(event.target.getAttribute('data-start'));
+  vid.setCurrentTime(captionStart);
+  vid.play();
 });
-// const span = document.querySelectorAll('span');
-// //check methods on video element
-// span.addEventListener('click', () => {
-//
-//     caption.classList.add("highlight");
-//
-//     let start = caption.dataset.start;
-//     vid.currentTime = start;
-//   });
-//
-
-
-
-
-
-
-  //   //start back here
-  //   //found how to make them both numbers, now I need to make the span clickable
-  //   video.currentTime == Number(spanTime);
-  //   console.log(spanTime);
-  // }
-  //
-  // });
